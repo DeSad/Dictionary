95faa3f6d385b3875be6982e8464a9ef39a89d05
Stanislav <stas.kozhemyako@gmail.com>
2016-07-05 17:51:28 +0300

    ended

---
+++ Dictionary/Dictionary/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Dictionary
+//
+//  Created by Stanislav Kozhemyako on 7/5/16.
+//  Copyright © 2016 Stanislav Kozhemyako. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Dictionary/Dictionary/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Dictionary
+//
+//  Created by Stanislav Kozhemyako on 7/5/16.
+//  Copyright © 2016 Stanislav Kozhemyako. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
